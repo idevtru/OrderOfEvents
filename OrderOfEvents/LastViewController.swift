73ef11d7b72d6914a7e61f42bad3c1d1c42fd236
Rich Castagna <idev.tru@gmail.com>
2018-12-16 15:40:20 -0500

    App Development with Swift, Lesson 3.8 View Controller Life Cycle

---
+++ OrderOfEvents/LastViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LastViewController.swift
+//  OrderOfEvents
+//
+//  Created by Richard Castagna on 12/16/18.
+//  Copyright © 2018 Richard Castagna. All rights reserved.
+//
+
+import UIKit
+
+class LastViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
